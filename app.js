67775a9850210748f753f787cd462a4a45fe61cc
imanabdurrohman <2200016112@webmail.uad.ac.id>
2024-12-22 21:23:30 +0700

    Webhooks test

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 
 app.use(express.json());
 
-// Endpoint utama
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// Endpoint untuk menerima data
 app.post('/data', (req, res) => {
     const { name, age } = req.body;
     if (!name || !age) {
@@ -17,7 +15,6 @@ app.post('/data', (req, res) => {
     res.status(201).send({ name, age });
 });
 
-// Endpoint untuk mengembalikan data berdasarkan ID
 app.get('/data/:id', (req, res) => {
     const id = req.params.id;
     res.status(200).send({ id, name: 'Test User', age: 25 });
