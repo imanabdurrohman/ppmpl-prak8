cd724f0475c890e639207d9965bce1fb37f552c8
imanabdurrohman <2200016112@webmail.uad.ac.id>
2024-12-24 17:47:12 +0700

    update code

--- utils.js
+++
@@ -1,9 +0,0 @@
-function processData(data, id) {
-  return { id, ...data };
-}
-
-function validateInput(data) {
-  return data.name && data.age ? true : false;
-}
-
-module.exports = { processData, validateInput };
